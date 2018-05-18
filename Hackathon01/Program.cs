a7e7f09312b25a3ae0ff7a86fccab7c90a0f6246
育瑄 張 <kellyjhang8329@gmail.com>
2018-05-18 13:41:33 +0800

    更新

---
+++ Hackathon01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int x = 1; x < 16; x++)//公雞
+            //{
+            //    for (int y = 1; y < 32; y++)//母雞
+            //    {
+            //        for (int z = 1; z < 100; z++)//小雞
+            //        {
+            //            if (x + y + z == 100 && ((59 * x) + (29 * y)) == 900)
+            //            {
+            //                Console.WriteLine("公雞：" + x);
+            //                Console.WriteLine("母雞：" + y);
+            //                Console.WriteLine("小雞：" + z);
+            //            }
+
+            //        }
+            //    }
+            //}
+            int x = 1;
+            int y = (59 + x);
+            int z = (29 + x);
+            for (int i = 1; i <= 100; i++)
+            {
+                if (x + y + z == 100 && ((59 * x) + (29 * y)) == 900)
+                {
+                    Console.WriteLine("公雞：" + x);
+                    Console.WriteLine("母雞：" + y);
+                    Console.WriteLine("小雞：" + z);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hackathon02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 101; i < 200; i++)
+            {
+                if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0 && i % 13 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
